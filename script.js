36694a9a5e61033deea39012350f99ed50a31b27
Anastasia S. <annihilcax@gmail.com>
2026-01-30 15:30:18 +0300

    completed task

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
-let title;
-let screens;
-let screenPrice;
-let rollback;
-let fullPrice;
-let adaptive;
+let title = "Название проекта (под редакцией)";
+let screens = "Простые, Сложные, Интерактивные";
+let screenPrice = 3000;
+let rollback = 10;
+let fullPrice = 12000;
+let adaptive = false;
 
-alert("Сообщение с любым текстом");
-console.log("Сообщение с любым текстом");
+console.log(typeof title, typeof fullPrice, typeof adaptive);
+console.log(screens.length);
+
+console.log("Стоимость верстки экранов " + screenPrice + " рублей");
+console.log("Стоимость разработки сайта " + fullPrice + " рублей");
+
+screens = screens.toLowerCase().split(", ");
+console.log(screens);
+
+console.log(fullPrice * (rollback / 100));
