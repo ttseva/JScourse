b70a54d94380456a363e6abfe4cce6c71c9c435d
Anastasia S. <annihilcax@gmail.com>
2026-02-04 02:20:24 +0300

    lesson completed

--- script.js
+++ script.js
@@ -2,16 +2,30 @@
 
 let rollback = 10;
 
-let title = prompt("Как называется ваш проект?")
-let screens = prompt("Какие типы экранов нужно разработать? (Простые/Сложные/Адаптивные)", "Простые")
-let screenPrice = +prompt("Сколько будет стоить данная работа?")
-let adaptive = prompt("Нужен ли адаптив на сайте? (Да/Нет)", "Нет") === "Да"
+let title = prompt("Как называется ваш проект?");
+let screens = prompt("Какие типы экранов нужно разработать? (Простые/Сложные/Адаптивные)", "Простые");
+let screenPrice = +prompt("Сколько будет стоить данная работа?");
+let adaptive = prompt("Нужен ли адаптив на сайте? (Да/Нет)", "Нет") === "Да";
 
-let service1 = prompt("Какой дополнительный тип услуги нужен?")
-let servicePrice1 = +prompt("Сколько это будет стоить?")
-let service2 = prompt("Какой дополнительный тип услуги нужен?")
-let servicePrice2 = +prompt("Сколько это будет стоить?")
+let service1 = prompt("Какой дополнительный тип услуги нужен?");
+let servicePrice1 = +prompt("Сколько это будет стоить?");
+let service2 = prompt("Какой дополнительный тип услуги нужен?");
+let servicePrice2 = +prompt("Сколько это будет стоить?");
 
+let fullPrice = screenPrice + servicePrice1 + servicePrice2;
+
+let servicePercentPrice = Math.ceil(fullPrice - fullPrice * (rollback / 100));
+console.log(servicePercentPrice);
+
+if (fullPrice >= 30000) {
+  console.log("Даем скидку в 10%");
+} else if (fullPrice < 30000 && fullPrice >= 15000) {
+  console.log("Даем скидку в 5%");
+} else if (fullPrice < 15000 && fullPrice > 0) {
+  console.log("Скидка не предусмотрена");
+} else {
+  console.log("Что-то пошло не так.");
+}
 
 
 console.log(typeof title, typeof fullPrice, typeof adaptive);
