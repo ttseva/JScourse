107ca107259525f363042a369df1d78a2143fb4d
Anastasia S. <annihilcax@gmail.com>
2026-02-07 13:39:19 +0300

    complete lesson 08-1

--- script.js
+++ script.js
@@ -44,7 +44,7 @@ const appData = {
       let price = 0;
 
       do {
-        name = prompt('Какой дополнительный тип услуги нужен?');
+        name = prompt('Какой дополнительный тип услуги нужен?') + `_${i}`;
       } while (appData.isNumber(name));
       do {
         price = prompt('Сколько это будет стоить?');
@@ -56,9 +56,13 @@ const appData = {
   },
 
   addPrices: function () {
-    for (let screen of appData.screens) {
-      appData.screenPrice += +screen.price;
-    }
+    appData.screenPrice = appData.screens.reduce(function (sum, screen) {
+      return sum + +screen.price;
+    }, 0)
+    // for (let screen of appData.screens) {
+    //   appData.screenPrice += +screen.price;
+    // }
+
     for (let key in appData.services) {
       appData.allServicePrices += appData.services[key];
     }
