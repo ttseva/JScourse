ba70109382fc7fb8f1dbbc4122cfce2d515bec48
Anastasia S. <annihilcax@gmail.com>
2026-02-14 15:24:39 +0300

    refactor lesson15

--- others/dom-element/script.js
+++ others/dom-element/script.js
@@ -9,23 +9,21 @@ const DomElement = function (selector, height, width, bg, fontSize) {
   this.width = width;
   this.bg = bg
   this.fontSize = fontSize
+}
 
-  this.addElement = function (text) {
-    if (this.selector[0] === '.') {
-      const newDiv = document.createElement('div');
-      newDiv.classList.add(this.selector.slice(1));
-      newDiv.style.cssText = `height: ${this.height}; width: ${this.width}; background-color: ${this.bg}; font-size: ${this.fontSize}`;
-      newDiv.textContent = text;
-      body.append(newDiv);
-      
-    } else if (this.selector[0] === '#') {
-      const newP = document.createElement('p');
-      newP.id = this.selector.slice(1);
-      newP.style.cssText = `height: ${this.height}; width: ${this.width}; background-color: ${this.bg}; font-size: ${this.fontSize}`;
-      newP.textContent = text;
-      body.append(newP);
-    }
+DomElement.prototype.addElement = function (text) {
+  let newElem;
+  if (this.selector[0] === '.') {
+    newElem = document.createElement('div');
+    newElem.classList.add(this.selector.slice(1));
+  } else if (this.selector[0] === '#') {
+    newElem = document.createElement('p');
+    newElem.id = this.selector.slice(1);
   }
+
+  newElem.style.cssText = `height: ${this.height}; width: ${this.width}; background-color: ${this.bg}; font-size: ${this.fontSize}`;
+  newElem.textContent = text;
+  body.append(newElem);
 }
 
 const block1 = new DomElement('.block', '100px', '100px', 'red', '16px');
