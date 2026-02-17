da92b5e54153ea96290a118b2d70fb75189dbf7c
Anastasia S. <annihilcax@gmail.com>
2026-02-17 10:06:50 +0300

    complete lesson 16 (see /others/proto-method)

---
+++ others/proto-method/script.js
@@ -0,0 +1,17 @@
+'use strict'
+
+class First {
+  hello() {
+    console.log('Привет я метод родителя!');
+  }
+}
+
+class Second extends First {
+  hello(){
+    super.hello();
+    console.log('А я наследуемый метод!')
+  }
+}
+
+const obj = new Second();
+obj.hello();
